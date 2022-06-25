d5623b9e8b7e8c2b10691222b14388464bd9efe8
raviaakash1 <raviaakash1@gmail.com>
2022-06-25 21:52:34 +0530

    18th Exercise

--- ex18.py
+++ ex18.py
@@ -6,3 +6,22 @@
 #       3. Using 1 and 2, they let you make your own "mini-script" or "tiny commands".
 
 #  Create functions using dev keyword
+
+
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1 : {arg1}  arg2 : {arg2}")
+
+def print_two_again(arg1,arg2):
+    print(f"arg1 : {arg1}  arg2 : {arg2}")
+
+def print_one(arg1):
+    print(f"arg1 : {arg1}")
+
+def print_none():
+        print("I got nothing")
+
+print_two("Aakash","Ravi")
+print_two_again("Ravi","Aakash")
+print_one("Ravi")
+print()
